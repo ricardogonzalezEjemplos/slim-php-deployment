2e1268e381d5441740bb938aa4f58e9ff2ca412a
ricardogonzalezEjemplos <149983151+ricardogonzalezEjemplos@users.noreply.github.com>
2023-11-05 23:01:00 -0300

    Update index.php

--- app/index.php
+++ app/index.php
@@ -21,7 +21,7 @@ $app->addBodyParsingMiddleware();
 
 // Routes
 $app->get('[/]', function (Request $request, Response $response) {
-    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework 2023"));
+    $payload = json_encode(array('method' => 'GET', 'msg' => "Bienvenido a SlimFramework "));
     $response->getBody()->write($payload);
     return $response->withHeader('Content-Type', 'application/json');
 });
